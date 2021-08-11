b64938a7d60f6e43dfd680da41d76c6f66a671d0
Tyler <tylerclancy030@gmail.com>
2021-08-11 15:19:54 -0500

    Add js fetching

---
+++ script.js
@@ -0,0 +1,19 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {}
+
+// Get quotes from API
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On load
+getQuotes();
