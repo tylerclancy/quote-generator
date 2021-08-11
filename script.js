399f82b40e1fc65cf6e8ec0b44c5d8ac4af1f888
Tyler <tylerclancy030@gmail.com>
2021-08-11 18:58:12 -0500

    Add loader

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const quoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loader
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if author is null
@@ -23,11 +37,14 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
 
+  // Set quote and hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
